f18fc8829ae84da80868cd685a7b9c76ab2ebde7
Artem Sabadash <76249593+timdvd@users.noreply.github.com>
2025-01-21 16:12:20 +0200

    Update script.js

--- 3_5.DOM_navigation/js/script.js
+++ 3_5.DOM_navigation/js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+console.log(document.head);
+console.log(document.documentElement);
+console.log(document.body.childNodes);
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
+
+console.log(document.querySelector('#current').parentNode);
+console.log(document.querySelector('#current').parentElement);
+console.log(document.querySelector('[data-current="3"]').nextSibling);
+console.log(document.querySelector('[data-current="3"]').previousSibling);
+
+for(let node of document.body.childNodes){
+  if(node.nodeName == '#text') {
+    continue;
+  }
+}
