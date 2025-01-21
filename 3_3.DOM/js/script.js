2e1bfdc24a0bc5a7e616e7ec11c3ca8475003030
Artem Sabadash <76249593+timdvd@users.noreply.github.com>
2025-01-21 11:48:34 +0200

    Update script.js

--- 3_3.DOM/js/script.js
+++ 3_3.DOM/js/script.js
@@ -1,2 +1,26 @@
 'use strict';
 
+const box = documeny.getElementById('box');
+console.log('box');
+
+const btn1 = document.getElementsByTagName('button')[0];
+const btn2 = document.getElementsByTagName('button')[1];
+
+const btns = document.getElementsByTagName('button');
+console.log(btns');
+console.log(btns[0]);
+console.log(btns[1]);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+console.log(hearts);
+
+hearts.forEach(item => {
+  console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
