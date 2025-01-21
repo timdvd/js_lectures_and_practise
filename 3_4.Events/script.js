6f9cf80784739602e526915c92a06233f02dd6f9
Artem Sabadash <76249593+timdvd@users.noreply.github.com>
2025-01-21 15:05:10 +0200

    Update script.js

--- 3_4.Events/script.js
+++ 3_4.Events/script.js
@@ -1,9 +1,41 @@
 const btn = document.querySelector('button');
+const btn2 = document.querySelectorAll('button')[1];
 
 btn.onclick = function(){
     alert('Click');
 }
 
 btn.addEventListener('click', () => {
-    
-})
+    alert('Click123');
+});
+
+btn.addEventListener('mouseenter', (e) => {
+    console.log(e.target);
+    e.target.remove();
+    alert('hover');
+});
+
+const deleteElement = (e) => {
+    e.target.remove();
+};
+
+let i = 0;
+const deleteElement = (e) => {
+    console.log(e.target);
+    i++;
+    if(i == 1){
+        btn2.removeEventListener('click', deleteElement);
+    }
+};
+
+btn2.addEventListener('click', deleteElement);
+
+btns.forEach(btn => {
+    btn.addEvemtListener('click', deleteElement, {once: true});
+});
+
+const link = document.querySelector('a');
+link.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(event.target);
+});
